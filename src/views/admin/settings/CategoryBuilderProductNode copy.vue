ac4c517d774eb38c4d2da81945b0da9e18325b59
ABU BAKAR SIDDIQUE <abubakar.siddique@annanovas.net>
2025-07-17 18:59:04 +0600

    Category search updated

---
+++ src/views/admin/settings/CategoryBuilderProductNode copy.vue
@@ -0,0 +1,83 @@
+<script setup>
+const props = defineProps({
+  node: {
+    type: Object,
+    required: false,
+    default: () => ({}),
+  },
+  selected: {
+    type: Array,
+    required: false,
+    default: () => ([]),
+  },
+  indeterminate: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+})
+
+const emit = defineEmits(['toggle-select'])
+
+const getNodeId = node => node.realId || node._id
+
+// Computed checkbox state based on selected prop
+const isChecked = computed({
+  get() {
+    return props.selected.includes(getNodeId(props.node))
+  },
+  set(value) {
+    emit('toggle-select', { id: getNodeId(props.node), checked: value })
+  },
+})
+
+
+// Ref to the checkbox component for setting indeterminate
+const checkbox = ref(null)
+
+// Watch and set indeterminate property on the actual checkbox input element
+watch(
+  () => props.indeterminate,
+  newVal => {
+    if (checkbox.value) {
+      checkbox.value.indeterminate = newVal
+    }
+  },
+  { immediate: true },
+)
+
+// Emit toggle-select on checkbox change event
+function onCheckboxChange(val) {
+  emit('toggle-select', { id: getNodeId(props.node), checked: val })
+}
+</script>
+
+<template>
+  <div class="mb-2 px-2 py-1 bg-grey-lighten-4 rounded">
+    <div class="d-flex justify-between align-center">
+      <div class="d-flex align-center">
+        <VCheckbox
+          v-model="isChecked"
+          hide-details
+          dense
+          class="me-2"
+          :indeterminate="indeterminate"
+          @update:model-value="onCheckboxChange"
+        />
+        <strong>{{ node.name }}</strong>
+      </div>
+    </div>
+    <div
+      v-if="node.children?.length"
+      class="ms-4 mt-2"
+    >
+      <CategoryBuilderProductNode
+        v-for="child in node.children"
+        :key="child.realId"
+        :node="child"
+        :selected="selected"
+        @toggle-select="$emit('toggle-select', $event)"
+      />
+    </div>
+  </div>
+</template>
