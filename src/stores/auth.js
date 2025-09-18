c6716b2548e22e86217bf4a01bba66978b67e032
ABU BAKAR SIDDIQUE <abubakar.siddique@annanovas.net>
2025-09-18 14:49:38 +0600

    Role permission wise action added

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -62,7 +62,7 @@ export const useAuthStore = defineStore('auth', {
 
     updateStation(userData) {
       if (this.fuserData) {
-        this.fuserData = userData
+        this.fuserData.stationID = userData.stationID
         localStorage.setItem('fuserData', JSON.stringify(this.fuserData))
       }
     },
