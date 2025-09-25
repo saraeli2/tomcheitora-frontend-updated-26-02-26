2c30b075f7610f0f3d1decb776ccf0a6474b53d1
ABU BAKAR SIDDIQUE <abubakar.siddique@annanovas.net>
2025-09-25 16:09:21 +0600

    Text updated

--- src/pages/profile/orders/index.vue
+++ src/pages/profile/orders/index.vue
@@ -111,7 +111,7 @@ const hasReceivable = computed(() => ordersData.value.hasReceivable)
 
                 <span v-if="hasReceivable">
                   <RouterLink :to="`/profile/received-weight-update`" style="background: rgb(255 91 24);color: #fff !important;margin: 0 10px;display: inline-block;padding: 4px 10px;border-radius: 3px;">
-                    {{ $t('משקל שקיבלתי') }}
+                    {{ $t('עדכון משקלים') }}
                   </RouterLink>
                 </span>
               </h5>
